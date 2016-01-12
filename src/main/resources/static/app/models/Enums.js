586c39dc0fd1f74896e208caa7d30e31f7ecd0bd
Cameron <cmcnaught5002@gmail.com>
2016-01-11 19:26:10 -0700

    Add "About" Page

---
+++ src/main/resources/static/app/models/Enums.js
@@ -0,0 +1,9 @@
+'use strict';
+
+myApp.constant('enums',{
+    Status: {
+        New: {display:"New",value:1},
+        Active:{display:"Active",value:2},
+        Inactive:{display:"Inactive",value:3}
+    }
+});
