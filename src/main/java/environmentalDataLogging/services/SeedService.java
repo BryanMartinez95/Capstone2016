16aad4cc0a8db8fe259223bbe60d4b51fc4afad3
unknown <645111@ELLPIT206033.ACDM.DS.SAIT.CA>
2016-01-15 09:06:36 -0700

    no message

--- src/main/java/environmentalDataLogging/services/SeedService.java
+++ src/main/java/environmentalDataLogging/services/SeedService.java
@@ -20,8 +20,8 @@ public class SeedService extends BaseService
 	{
 
 
-		User admin = new User("Admin", "Admin", "admin@gmail.com", Status.ACTIVE, "password", RoleType.ADMIN);
-		User user = new User("Fred", "Wilson", "fredwilson@gmail.com", Status.ACTIVE, "password", RoleType.USER);
+		User admin = new User("Admin", "Admin", "admin@gmail.com", "password", Status.ACTIVE, RoleType.ADMIN);
+		User user = new User("Fred", "Wilson", "fredwilson@gmail.com", "password", Status.ACTIVE, RoleType.USER);
 		userRepository.saveAndFlush(admin);
 		userRepository.saveAndFlush(user);
 
