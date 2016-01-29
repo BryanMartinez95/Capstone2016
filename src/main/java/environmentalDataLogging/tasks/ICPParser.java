baab58a5bbc1bb274f16214080362b50053af82d
Bryan Martinez <b.martinez.95@hotmail.com>
2016-01-29 13:28:57 -0700

    more user seed's

---
+++ src/main/java/environmentalDataLogging/tasks/ICPParser.java
@@ -0,0 +1,24 @@
+package environmentalDataLogging.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 631152 on 1/28/2016.
+ */
+public class ICPParser
+{
+    private List<String> header;
+    public void setHeader()
+    {
+        header = new ArrayList();
+        for (String val: header)
+        {
+            System.out.println(val);
+        }
+
+    }
+    public void parse()
+    {
+    }
+}
