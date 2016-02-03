9ef043f9d8d025e9927dd39048a800d6a950bafc
Cameron <cmcnaught5002@gmail.com>
2016-02-02 23:34:34 -0700

    Fixed basic pagination on grid

--- src/main/resources/static/app/controllers/GridController.js
+++ src/main/resources/static/app/controllers/GridController.js
@@ -1,14 +1,6 @@
 'use strict';
 
-angular.module('appController').controller('GridController', function($scope, SingleSelect, GridRequestModel, Enum){
-
-    /**
-     * Locally holds all the data in the grid currently on the page.
-     * Data will be set by the directive.
-     * @type {Array}
-     */
-    $scope.gridData = [];
-
+angular.module('appController').controller('GridController', function($scope, SingleSelect, GridRequestModel, GridResultModel, Enum){
     /**
      * Options to display in the single-select for number of items per page in the grid.
      * @type {Array}
@@ -47,6 +39,7 @@ angular.module('appController').controller('GridController', function($scope, Si
      */
     function updateGrid(model) {
         var options = GridRequestModel.newGridRequestModel();
+
         options.pageSize = model.pageSize || $scope.perPage.value;
         options.currentPage = model.currentPage || $scope.currentGridPage;
         options.pages = model.pages || $scope.paginationPages;
@@ -54,22 +47,25 @@ angular.module('appController').controller('GridController', function($scope, Si
         options.sortBy = model.sortBy || $scope.sortBy;
         options.lastPage = model.lastPage || $scope.lastGridPage;
 
-        //UserService.findAll().then(function(resp) {
-        //    var data = GridRequestModel.newGridRequestModelFromJson(resp.data);
-        //    for (var item in SingleSelect.GridSize) {
-        //        if (item.value == data.pageSize) {
-        //            $scope.perPage = item;
-        //        }
-        //    }
-        //    $scope.currentGridPage = data.currentPage;
-        //    $scope.paginationPages = data.pages;
-        //    $scope.filters = data.filters;
-        //    $scope.sortBy = data.sortBy;
-        //    $scope.lastGridPage = data.lastPage;
-        //});
+        $scope.$parent.$parent.$parent.gridOptions = options;
+        $scope.$parent.$parent.$parent.GetGridData.then(function(resp) {
+            var data = GridResultModel.newGridResultModelFromJson(resp);
 
-        $scope.$apply();
+            for (var item in SingleSelect.GridSize) {
+                if (item.value == data.pageSize) {
+                    $scope.perPage = item;
+                }
+            }
+            $scope.currentGridPage = data.currentPage;
+            $scope.paginationPages = new Array(data.lastPage);
+            $scope.filters = data.filters;
+            $scope.sorts = data.sorts;
+            $scope.lastGridPage = data.lastPage;
+            $scope.gridData = data.list;
+        });
     };
+
+    $scope.gridData = [];
 
     $scope.goToNextPage = function(){
         var model = GridRequestModel.newGridRequestModel();
@@ -134,4 +130,24 @@ angular.module('appController').controller('GridController', function($scope, Si
     };
 
     $scope.defaultSortOrder = Enum.SortOrder.Ascending;
+
+    $scope.init = function() {
+        $scope.$parent.$parent.GetGridData.then(function(resp){
+            for (var item in SingleSelect.GridSize) {
+                if (item.value == resp.pageSize) {
+                    $scope.perPage = item;
+                }
+            }
+            $scope.currentGridPage = resp.currentPage;
+            $scope.paginationPages = [];
+            $scope.filters = resp.filters;
+            $scope.sorts = resp.sorts;
+            $scope.lastGridPage = resp.lastPage;
+            $scope.gridData = resp.list;
+            var counter = 1;
+            while(counter <= resp.lastPage) {
+                $scope.paginationPages.push(counter++);
+            }
+        });
+    };
 });
--- src/main/resources/static/app/directives/scripts/Grid.js
+++ src/main/resources/static/app/directives/scripts/Grid.js
@@ -6,32 +6,16 @@ angular.module('appDirective').directive('saitGrid', function(){
        templateUrl: 'app/directives/templates/grid.html',
        scope: true,
        link: function(scope, element, attrs) {
-           /**
-            * Access the controller immediately preceeding the GridController that
-            * has a $http service call.
-            */
-          scope.$parent.$parent.GetGridData.then(function(resp) {
-               var data = resp;
-               var headers = [];
-               /**
-                * Set the grid rows to the resp from the server
-                */
-               scope.rows = data;
-
-               /**
-                * Set the grid data locally in the controller to avoid additional
-                * database calls on sorting and pagination.
-                */
-               scope.$parent.gridData = data;
-
-               for (var key in data[0]){
-                   headers.push({
-                       display: key,
-                       sortOrder: scope.$parent.defaultSortOrder
-                   });
+           scope.$parent.$watch('gridData', function(newVal, oldVal){
+               if (newVal) {
+                   scope.rows = newVal;
+                   scope.headers = [];
+                   for (var key in scope.$parent.gridData[0]){
+                       scope.headers.push({
+                           display: key
+                       });
+                   }
                }
-
-               scope.headers = headers;
            });
 
        }
