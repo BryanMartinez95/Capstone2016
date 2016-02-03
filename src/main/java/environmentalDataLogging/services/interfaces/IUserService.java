257aa5840d077ee6d0464ad1767dd9e8c73b1393
Josh Lynn <joshlynn79@gmail.com>
2016-02-02 20:54:56 -0700

    Create GridRequestModel, GridResultModel Added Helpers to help with the pagination Added filter and sorting models Added SortType Enum

--- src/main/java/environmentalDataLogging/services/interfaces/IUserService.java
+++ src/main/java/environmentalDataLogging/services/interfaces/IUserService.java
@@ -1,10 +1,13 @@
 package environmentalDataLogging.services.interfaces;
 
 import environmentalDataLogging.entities.User;
+import environmentalDataLogging.models.grids.GridRequestModel;
+import environmentalDataLogging.models.grids.GridResultModel;
 import environmentalDataLogging.models.views.UserModel;
 
 public interface IUserService extends ICrudService<User, UserModel>
 {
 	UserModel findCurrentUser();
 	User findByEmail(String email);
+	GridResultModel getGridList(GridRequestModel gridRequestModel);
 }
