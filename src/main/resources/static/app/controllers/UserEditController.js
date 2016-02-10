5dcebf03ee08284b0d1af83e6eb8ffaf83390de8
lynnjoshua <joshlynn79@gmail.com>
2016-02-10 15:07:05 -0700

    Moved from state back to route

---
+++ src/main/resources/static/app/controllers/UserEditController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('appController').controller('UserEditController', function ($scope, UserService) {
+
+    var id = "id";
+
+    var user = UserService.findOne(id);
+
+    $scope.user.firstName = user.firstName;
+    $scope.user.lastName = user.lastName;
+    $scope.user.email = user.email;
+    $scope.user.password = user.password;
+
+    $scope.edit = function () {
+        console.log("Edit Button Pressed");
+        var user = new User();
+
+        user.firstName = $scope.user.firstName;
+        user.lastName = $scope.user.lastName;
+        user.password = $scope.user.password;
+        user.email = $scope.user.email;
+
+        console.log(user);
+
+        UserService.update(user);
+    };
+
+    $scope.cancel = function () {
+        console.log("Cancel Button Pressed");
+    };
+});
