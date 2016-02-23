a62331a67c7fb2a2246a1f84753a446f68f06823
Cameron <cmcnaught5002@gmail.com>
2016-02-23 01:18:21 -0500

    Started work on Grid Filter

--- src/main/resources/static/app/models/SingleSelect.js
+++ src/main/resources/static/app/models/SingleSelect.js
@@ -18,6 +18,11 @@ angular.module('appModel').factory('SingleSelect', function(Enum) {
         RoleType: [
             { display: Enum.RoleType.User.display, value: Enum.RoleType.User.value },
             { display: Enum.RoleType.Admin.display, value: Enum.RoleType.Admin.value }
+        ],
+        FilterType: [
+            { display: Enum.FilterType.StartsWith.display, value: Enum.FilterType.StartsWith.value },
+            { display: Enum.FilterType.EndsWith.display, value: Enum.FilterType.EndsWith.value },
+            { display: Enum.FilterType.Contains.display, value: Enum.FilterType.Contains.value }
         ]
     }
 });
