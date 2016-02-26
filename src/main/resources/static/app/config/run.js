91811c57ffa38d3a7077fb7e24e67de71a3cb7db
cmcnaught5002 <Fre3d0m2889>
2016-02-26 12:43:07 -0700

    Fixed single select working with dynamic data. See Main Project page for example

--- src/main/resources/static/app/config/run.js
+++ src/main/resources/static/app/config/run.js
@@ -3,8 +3,8 @@
 angular.module('app').run(function ($http, $rootScope, $location) {
     $rootScope.$watch(function() { return $location.path(); }, function(newValue, oldValue){
         if ($rootScope.authenticated == false){
-            console.log(newValue);
-            console.log(oldValue);
+            //console.log(newValue);
+            //console.log(oldValue);
             $location.path('/Login');
         }
     });
