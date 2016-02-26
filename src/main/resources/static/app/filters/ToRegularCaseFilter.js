73fed14c84f35ecaaa1daddae45c94818a6735e3
Alec Wassill <alec.wassill@gmail.com>
2016-02-26 10:03:10 -0700

    Fixed Grid toRegularCase filter for the grid, thank you Cameron.

--- src/main/resources/static/app/filters/ToRegularCaseFilter.js
+++ src/main/resources/static/app/filters/ToRegularCaseFilter.js
@@ -2,17 +2,8 @@
 
 angular.module('appFilter').filter('toRegularCase', function(){
     return function(str) {
-
-        if( str == null )
-            return null;
-        else if( typeof str === "string" || str instanceof String)
-        {
-            return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-                return index == 0 ? letter.toUpperCase() : letter.toLowerCase();
-            }).replace(/\s+/g, '');
-        }
-        else if( str.name != null )
-            return str.name;
-        else return str;
+        return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+            return index == 0 ? letter.toUpperCase() : letter.toLowerCase();
+        }).replace(/\s+/g, '');
     }
 });
