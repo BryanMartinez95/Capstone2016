4d1c06e433e2dcd1394a5835f48c3d6eb4613a98
Alec Wassill <alec.wassill@gmail.com>
2016-02-28 00:49:41 -0700

    Added Investigator Java controller and service

--- src/main/java/environmentalDataLogging/services/interfaces/IInvestigatorService.java
+++ src/main/java/environmentalDataLogging/services/interfaces/IInvestigatorService.java
@@ -1,10 +1,8 @@
 package environmentalDataLogging.services.interfaces;
 
-import environmentalDataLogging.entities.Client;
 import environmentalDataLogging.entities.Investigator;
 import environmentalDataLogging.models.grids.GridRequestModel;
 import environmentalDataLogging.models.grids.GridResultModel;
-import environmentalDataLogging.models.views.ClientModel;
 import environmentalDataLogging.models.views.InvestigatorModel;
 
 public interface IInvestigatorService extends ICrudService<Investigator, InvestigatorModel>
