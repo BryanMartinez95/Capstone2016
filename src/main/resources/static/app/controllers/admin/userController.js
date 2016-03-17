7e769228380db637c4b2c557e1cb7288abd856fb
unknown <645111@ELLPIT206033.ACDM.DS.SAIT.CA>
2016-03-17 16:12:28 -0600

    Working on measurements and status toggle

--- src/main/resources/static/app/controllers/admin/userController.js
+++ src/main/resources/static/app/controllers/admin/userController.js
@@ -6,6 +6,16 @@ angular.module('appController')
 
         $scope.setActiveService(UserService);
 
+	    $scope.data = {
+		    cb1: true,
+		    cb4: true,
+		    cb5: false
+	    };
+	    $scope.message = 'false';
+	    $scope.onChange = function (cbState) {
+		    $scope.message = cbState;
+	    };
+
         $scope.data = {};
         $scope.data.message = "User Overview Page";
 
@@ -141,4 +151,6 @@ angular.module('appController')
         function getStatusValue() {
             return $scope.isActive ? Enum.Status.Active.value : Enum.Status.Inactive.value;
         }
+
+
     });
