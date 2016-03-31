5584b530e363cbdebd41af89bce22fa5564bfad7
Josh Lynn <joshlynn79@gmail.com>
2016-03-31 14:57:09 -0600

    OMG

--- src/main/resources/static/app/config/run.js
+++ src/main/resources/static/app/config/run.js
@@ -1,27 +1,23 @@
 'use strict';
 
-angular.module('app').run(function ($rootScope, $location, AuthService, $http) {
+angular.module('app').run(function ($rootScope, $location, AuthService, $http, $timeout) {
 
     // AuthService.setUser();
 
-    $http.get('/Api/User/CurrentUser').success(function (data) {
-        if (!!data.email) {
-            $rootScope.isAuthenticated = true;
-        }
-        else {
-            $rootScope.isAuthenticated = false;
-        }
-        callback && callback();
-    });
-    
+    $timeout(function () {
+        $http.get('/Api/User/CurrentUser').success(function (data) {
+            $rootScope.isAuthenticated = !!data.email;
+        });
+    }, 200);
+
     AuthService.setUser();
 
     $rootScope.$on("$routeChangeStart", function (event, next, current) {
-            if (!$rootScope.isAuthenticated && next.auth) {
-                $location.path('/Login');
-            }
-            if (next.originalPath === '/Login' && $rootScope.isAuthenticated) {
-                $location.path(current.originalPath);
-            }
-        });
+        if (!$rootScope.isAuthenticated && next.auth) {
+            $location.path('/Login');
+        }
+        if (next.originalPath === '/Login' && $rootScope.isAuthenticated) {
+            $location.path(current.originalPath);
+        }
+    });
 });
